6aa9e951410c2b6e9a234659678d568589af0457
MNS <parsamanes@yahoo.com>
2024-10-24 11:47:40 +0300

    pre-carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,19 +47,84 @@ export default function Home() {
           </div>
         </div>
         <div className="bg-white">
-          <div className="flex justify-center items-center pt-20">
-          <Image
-              className="md:w-[60vw] md:h-auto w-[80vw]"
-              src="/image.png"
-              alt="model 2"
-              width={500}
-              height={500}
-              />
+          <div className="flex justify-center items-center md:pt-20 pt-10">
+            <Image
+                className="md:w-[60vw] md:h-auto w-[90vw]"
+                src="/image.png"
+                alt="model 2"
+                width={500}
+                height={500}
+                />
           </div>
-          <div>
-            
+          <div className="grid grid-rows-2 justify-center items-center md:mt-28 mt-10">
+            <h1 className="relative font-[700] md:text-[30px] text-default">مشاوران شما در سوپرپی</h1>
+            <div className="bg-[#F7415F] w-[78px] h-[5px] mx-auto -mt-2 md:-mt-5"></div>
           </div>
 
+          <Slider/>
+
+          <div className="grid grid-cols-3 px-14 gap-[20px] mt-28">
+            <div className="bg-transparent border-2 border-[#cfebfc] h-[413px] w-auto rounded-xl  " >
+              <div className="flex justify-center items-center -mt-20  ">
+                <Image
+                className="bg-gradient-to-r from-indigo-900 to-red-800 px-1 rounded-full w-[33%] shadow-md "
+                src='/model.png'
+                alt=''
+                width={150}
+                height={150}
+                            
+                />
+              </div>
+              <div className=" flex justify-center items-center">
+                <h1 className="mt-5 font-[500] text-[23px] " >سحر محمدی</h1>
+              </div>
+              <p className=" font-[400] text-default text-right text-[#4D4D4D] p-4 leading-[30px] " >دانشگاه آیدین، دانشگاهی خصوصی در شهر استانبول ترکیه است که در سال ۲۰۰۷ تأسیس شد. این دانشگاه یکی از بزرگترین دانشگاه‌های خصوصی ترکیه است و بیش از ۴۰ هزار دانشجو دارد و یکی از بهترین انتخاب‌ها برای تحصیل در ترکیه است</p>
+              <div className="grid grid-cols-2 justify-center items-center px-3 gap-3 mt-8" >
+                <button className="bg-[#CA5443] px-4 py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره تلفنی</button>
+                <button className="bg-[#CA5443] px-[12px] py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره واتساپ</button>
+              </div>
+            </div>
+            <div className="bg-transparent border-2 border-[#cfebfc] h-[413px] w-auto rounded-xl mt-24 " >
+              <div className="flex justify-center items-center -mt-20  ">
+                <Image
+                className="bg-gradient-to-r from-indigo-900 to-red-800 px-1 rounded-full w-[33%] shadow-md "
+                src='/model.png'
+                alt=''
+                width={150}
+                height={150}
+                            
+                />
+              </div>
+              <div className=" flex justify-center items-center">
+                <h1 className="mt-5 font-[500] text-[23px] " >سحر محمدی</h1>
+              </div>
+              <p className=" font-[400] text-default text-right text-[#4D4D4D] p-4 leading-[30px] " >دانشگاه آیدین، دانشگاهی خصوصی در شهر استانبول ترکیه است که در سال ۲۰۰۷ تأسیس شد. این دانشگاه یکی از بزرگترین دانشگاه‌های خصوصی ترکیه است و بیش از ۴۰ هزار دانشجو دارد و یکی از بهترین انتخاب‌ها برای تحصیل در ترکیه است</p>
+              <div className="grid grid-cols-2 justify-center items-center px-3 gap-3 mt-8" >
+                <button className="bg-[#CA5443] px-4 py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره تلفنی</button>
+                <button className="bg-[#CA5443] px-[12px] py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره واتساپ</button>
+              </div>
+            </div>
+            <div className="bg-transparent border-2 border-[#cfebfc] h-[413px] w-auto rounded-xl mt-24 " >
+              <div className="flex justify-center items-center -mt-20  ">
+                <Image
+                className="bg-gradient-to-r from-indigo-900 to-red-800 px-1 rounded-full w-[33%] shadow-md "
+                src='/model.png'
+                alt=''
+                width={150}
+                height={150}
+                            
+                />
+              </div>
+              <div className=" flex justify-center items-center">
+                <h1 className="mt-5 font-[500] text-[23px] " >سحر محمدی</h1>
+              </div>
+              <p className=" font-[400] text-default text-right text-[#4D4D4D] p-4 leading-[30px] " >دانشگاه آیدین، دانشگاهی خصوصی در شهر استانبول ترکیه است که در سال ۲۰۰۷ تأسیس شد. این دانشگاه یکی از بزرگترین دانشگاه‌های خصوصی ترکیه است و بیش از ۴۰ هزار دانشجو دارد و یکی از بهترین انتخاب‌ها برای تحصیل در ترکیه است</p>
+              <div className="grid grid-cols-2 justify-center items-center px-3 gap-3 mt-8" >
+                <button className="bg-[#CA5443] px-4 py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره تلفنی</button>
+                <button className="bg-[#CA5443] px-[12px] py-3 text-white font-bold text-default rounded-xl " >دریافت مشاوره واتساپ</button>
+              </div>
+            </div>
+          </div>
 
 
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		fontSize:{
+			default : "16px"
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
